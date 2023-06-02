e4ccb5d5e5c32a5198424d032baed906f6602709
David Carranza <deibydcarranza@outllok.com>
2023-06-02 16:48:10 -0600

    Generando receta

--- algorithms/regressionLog/urls.py
+++ algorithms/regressionLog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from .views import regression_algorithm
+from django.views.generic import TemplateView
+
 
 #Importar Dashboards
 from . import (dash_app)
 
 urlpatterns = [
-    path('', regression_algorithm, name="regression_path")
+    path('', regression_algorithm, name="regression_path"),
+    path('receta_cancer/', TemplateView.as_view(template_name='receta_cancer.html'), name='receta_C'),
 ]
