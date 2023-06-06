f4a938af1dac596a778a6e597a85387b6ad65df1
DiegoAndresRa <diego.unam.15@gmail.com>
2023-06-06 17:47:53 -0600

    Clustering

--- algorithms/cluster/tool.py
+++ algorithms/cluster/tool.py
@@ -2,8 +2,10 @@ import dash
 from dash import dash_table,dcc,html
 import base64
 import pandas as pd
-import os
+import numpy as np
 from .. import components as comp
+import plotly.express as px
+
 
 """ Save data on file, generete dataframe and return dash_tabe """
 def parse_contents(contents, filename,path_file):
@@ -18,16 +20,15 @@ def parse_contents(contents, filename,path_file):
             df = pd.read_csv(path_file) 
             # Generate html component
             render = render_results(df)
-            return render
+            print(render)
+            return render,df 
     except Exception as e:
         print(e)
         return html.Div([
             'Archivo erroneo, solo archivos .csv'
         ])
 
-    return render
-
-
+    return render,None
 
 """ Generate table """
 def render_results(df):
@@ -50,3 +51,44 @@ def render_results(df):
 def write_on_file(decoded,path_file):
     with open(path_file, 'w') as f:
             f.write(decoded.decode("utf-8"))
+
+def convert_to_dataframe(data):
+    if isinstance(data, pd.DataFrame):
+        return data
+    elif isinstance(data, np.ndarray):
+        return pd.DataFrame(data)
+    else:
+        raise ValueError("El parámetro de entrada no es un DataFrame ni un arreglo de NumPy.")
+
+
+def interactive_pairplot(df):
+    fig = px.scatter_matrix(df, dimensions=df.columns, color='comprar')   
+    fig.update_traces(marker=dict(size=3)) 
+    fig.update_layout(
+        height=800,
+        width = 600,
+        title='Densidad y gráficas de dispersión',
+        font=dict(size=10),
+        plot_bgcolor='#F9F9F9',
+        paper_bgcolor='#F9F9F9',
+        showlegend=True,
+        hovermode='closest',
+        hoverlabel=dict(bgcolor="white", font_size=10),
+        xaxis=dict(showgrid=False, zeroline=False, visible=False),
+        yaxis=dict(showgrid=False, zeroline=False, visible=False),
+    )
+    
+    return dcc.Graph(figure=fig)
+
+def extract_titles_columns(df):
+    column_names = df.columns.tolist()
+    print(column_names)
+    return column_names
+
+def select_items_df(array_items):
+    items_select = [{"value": item, "label": item} for item in array_items]
+    return items_select
+
+def drop_column(df,column_name):
+    df = df.drop(column_name,axis=1)
+    return df
